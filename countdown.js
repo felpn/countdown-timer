06b61636c186898df05fc6541df8e8fad6dd8cdf
Felipe M. Nery <felipemnery10@gmail.com>
2021-11-16 22:38:39 -0300

    countdown-christmas

---
+++ countdown.js
@@ -0,0 +1,26 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+const merryChristmas = "25 Dec 2021";
+
+function countdown() {
+    const merryChristmasDate = new Date(merryChristmas);
+    const currentDate = new Date();
+
+    const totalSeconds = new Date(merryChristmasDate - currentDate) / 1000;
+
+    const days = Math.floor (totalSeconds / 3600 / 24);
+    const hours = Math.floor (totalSeconds / 3600) % 24;
+    const mins = Math.floor (totalSeconds / 60) % 60;
+    const seconds = Math.floor (totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = mins;
+    secondsEl.innerHTML = seconds;
+}
+countdown();
+
+setInterval(countdown, 1000);
